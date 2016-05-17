6b1a45acaf52cf82e40fa6ce7f528bdcebb964d5
claus haslauer <claus.haslauer@uni-tuebingen.de>
2016-05-17 12:36:15 +0200

    updated to python 3.5 including UTF-8 decoding

--- wetter.py
+++ wetter.py
@@ -19,13 +19,12 @@ What this does
 
 revisions:
 20130517 -- fixed issue with string of prognose (http://stackoverflow.com/questions/16585674/multi-line-text-with-matplotlib-gridspec)
-20150903 -- fixed
-                - only 2 prognosis maps available now from Dutch weather service (instead of 3)
-                - the times when the script is run are a bit more simplified (maybe)
+201505   -- fixed KMNI service update
 20151103 -- updated DWD service update
+20160517 -- updated to python 3.5 including UTF-8 decoding
 """
 import sys
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import datetime
 import time
 import os
@@ -35,7 +34,7 @@ from matplotlib import cm #, colors
 from PIL import Image
 import numpy as np
 import re
-import HTMLParser
+import html.parser
 import sched
 
 def main():
@@ -47,7 +46,9 @@ def main():
     times_to_run = gen_times_to_run(start='today', stop='in 21 days', delta='6 hours')
 
     # TODO fix that this points to a chosen location
-    output_path = r'E:\Dropbox\segeln_spanien\out'
+    # segeln_spanien directory
+    ssdir = os.path.split(os.path.dirname(__file__))[0]
+    output_path = os.path.join(ssdir, 'out')
     #output_path = r'F:/weather_out'
 
     # if set to FALSE it will overwrite individual images every time
@@ -58,34 +59,35 @@ def main():
     # ------------------------------------------------------------
     # START of Script
 
-    print("The Python version is %s.%s.%s" % sys.version_info[:3])
+    print(("The Python version is %s.%s.%s" % sys.version_info[:3]))
 
     now = datetime.datetime.now()
-    print "script started at: ", now
-    print "  first image at: ", times_to_run[0]
-    print "  final image at: ", times_to_run[-1]
+    print("script started at: ", now)
+    print("  first image at: ", times_to_run[0])
+    print("  final image at: ", times_to_run[-1])
 
     n_runs = len(times_to_run)
 
     # loop over all selected times when a map is to be created
     for cur_i, time_to_run in enumerate(times_to_run):
-        print "\n-------------------------------------------"
-        print 'next run at: ', time_to_run
+        print("\n-------------------------------------------")
+        print('next run at: ', time_to_run)
         n_remaining = n_runs - (cur_i + 1)
-        print 'after this, there are %i runs remaining' % n_remaining
+        print('after this, there are %i runs remaining' % n_remaining)
+        #to_run_sec = time.mktime(datetime.datetime.strptime(time_to_run, "%d-%m-%Y_%H:%M").timetuple())
         to_run_sec = time.mktime(time_to_run.timetuple())
         sche = sched.scheduler(time.time, time.sleep)
-        time_to_run = to_run_sec
-        sche.enterabs(time_to_run, 1, create_grosswetterlage_overview_map, (output_path,save_individual_imgs))
+        time_to_run = to_run_sec #now + 5 #datetime.timedelta(seconds=5)
+        sche.enterabs(time_to_run, 1, create_grosswetterlage_overview_map, (output_path, save_individual_imgs))
 
         sche.run()
 
-    print "Done execution of wetter.py"
+    print("Done execution of wetter.py")
 
 
 def create_grosswetterlage_overview_map(img_path, save_individual_imgs):
 
-    print "start creating map"
+    print("start creating map")
 
     timestamp = time.strftime('%Y_%m_%d_%H_%M_%S')
     cur_yr = time.strftime('%Y')
@@ -128,7 +130,7 @@ def create_grosswetterlage_overview_map(img_path, save_individual_imgs):
     base_url_kmni = 'http://cdn.knmi.nl/knmi/map/page/weer/waarschuwingen_verwachtingen/weerkaarten/%s_large.gif'
     knmi_urls = []
     for cur_url_id in list_cur_KMNI_ids:
-        knmi_urls.append(base_url_kmni % cur_url_id)
+        knmi_urls.append(base_url_kmni % cur_url_id.decode("utf-8"))
 
     # the second las and last number indicate plotting position:
     #     the second last number: column id
@@ -153,11 +155,11 @@ def create_grosswetterlage_overview_map(img_path, save_individual_imgs):
     ## -----------
     ## load images
     tmp_lst_imgs = []
-    print 'loading maps: ',
+    print('loading maps: ', end=' ')
     for cur_url_id in dict_of_urls:
         if cur_url_id[0] == 'infoBox':
             continue
-        print '...', cur_url_id[0],
+        print('...', cur_url_id[0], end=' ')
         if cur_url_id[0][:4]=="KNMI":
             file_extension = 'gif'
         else:
@@ -171,14 +173,15 @@ def create_grosswetterlage_overview_map(img_path, save_individual_imgs):
 
         img_dst = os.path.join(img_path, picString)
 
-        imgRequest = urllib2.Request(cur_url_id[1])
-        imgData = urllib2.urlopen(imgRequest).read()
+        imgRequest = urllib.request.Request(cur_url_id[1])
+        imgData = urllib.request.urlopen(imgRequest).read()
 
         output = open(img_dst,'wb')
         output.write(imgData)
         output.close()
 
         tmp_lst_imgs.append(img_dst)
+
 
     ## --------------------
     ## make composite image
@@ -191,17 +194,16 @@ def create_grosswetterlage_overview_map(img_path, save_individual_imgs):
     n_rows = 5
     outer_grid = gridspec.GridSpec(n_rows, 3 )# ,wspace=0.0, hspace=0.0
 
-    print '\nmaking overview map: ',
+    print('\nmaking overview map: ', end=' ')
     for cur_map_id, map_dict in enumerate(dict_of_urls):
 
         cur_row = map_dict[-1] #(cur_map_id % n_rows)
         cur_column = map_dict[-2]
-        else:
 
-        print '...', map_dict[0],
+        print('...', map_dict[0], end=' ')
 
         # preparation: no axes
-        print "cur_row", cur_row
+        print("cur_row", cur_row)
         ax = plt.subplot(outer_grid[cur_row, cur_column], frameon=False)
         ax.axes.get_yaxis().set_visible(False)
         ax.axes.get_xaxis().set_visible(False)
@@ -214,7 +216,7 @@ def create_grosswetterlage_overview_map(img_path, save_individual_imgs):
         if map_dict[0] in ['wetter.net', 'KNMI_AL', 'KNMI_PL_0', 'KNMI_PL_1', 'KNMI_PL_2']: # , 'KNMI_PL_3'
             im = plt.imread(tmp_lst_imgs[cur_map_id])
             ax.imshow(im, origin='upper') #
-        elif map_dict[0] in ['dwd', 'dwd24', 'dwd36', 'dwd48', 'dwd84', 'dwd108']:
+        elif map_dict[0] in ['dwd', 'dwd24', 'dwd36', 'dwd48', 'dwd84']:
             im = Image.open(tmp_lst_imgs[cur_map_id])#.convert("L")
             ar = np.asarray(im)
             ax.imshow(ar) #, cmap='Greys_r'
@@ -227,13 +229,15 @@ def create_grosswetterlage_overview_map(img_path, save_individual_imgs):
         ax.set_title(map_dict[2], size=6)
         fig.add_subplot(ax)
 
+        cur_row += 1
+
     # TODO plot the text of the prognosis in reasonable way
     title_x_c = 0.5
     title_y_c = 0.95
     cur_date_time = time.strftime('%Y_%m_%d_%H_%M_%S')
     outfig_name = '_grosswetterlage_overview_' + cur_date_time + ".png"
     plt.savefig(os.path.join(img_path, outfig_name), dpi=300, bbox_inches="tight", pad_inches=0)
-    print "\ndone!"
+    print("\ndone!")
 
 def gen_times_to_run(start='today', stop='in 1 days', delta='6 hours'):
     # old conventions:
@@ -267,7 +271,7 @@ def gen_times_to_run(start='today', stop='in 1 days', delta='6 hours'):
         #print "matchObj.group(1) : ", matchObj.group(1)
         delta_days = int(matchObj.group(1))
     else:
-        print "No match!!"
+        print("No match!!")
         raise Exception
 
 
@@ -278,7 +282,7 @@ def gen_times_to_run(start='today', stop='in 1 days', delta='6 hours'):
         #print "matchObj.group(1) : ", matchObj.group(1)
         delta_hours = int(matchObj.group(1))
     else:
-        print "No match!!"
+        print("No match!!")
         raise Exception
 
     # end within delta_days, but add also the delta_hours so the final time is the one desired
@@ -304,23 +308,23 @@ def perdelta(start, end, delta):
 
 
 def get_gwl_string(url):
-    aResp = urllib2.urlopen(url)
+    aResp = urllib.request.urlopen(url)
     web_pg = aResp.read()
 
     # parse for title
-    re_title = '<h1>(Gro&szlig;wetterlage in Europa f&uuml;r den [0-9]*.[0-9]*.[0-9]*)</h1>'
+    re_title = b'<h1>(Gro&szlig;wetterlage in Europa f&uuml;r den [0-9]*.[0-9]*.[0-9]*)</h1>'
     title = reFind(re_title, web_pg)
-    print 'title: ', title
+    print('title: ', title)
 
     # parse for prognose
-    re_prognose = '<h2>Die aktuelle Wetterprognose zur Gro&szlig;wetterlage</h2>\s([\w\&\;\,\.\-\s]*)'
+    re_prognose = b'<h2>Die aktuelle Wetterprognose zur Gro&szlig;wetterlage</h2>\s([\w\&\;\,\.\-\s]*)'
     prognose = reFind(re_prognose, web_pg)
 
     #match 80 characters, plus some more, until a space is reached
     pattern = re.compile(r'(.{70}\w*?)(\s)')
     #keep the '80 characters, plus some more' and substitute the following space with new line
     prognose = pattern.sub(r'\1\n', prognose)
-    print 'prognose: ', prognose
+    print('prognose: ', prognose)
 
     return title, prognose
 
@@ -334,8 +338,8 @@ def reFind(re_string, txt_string):
     if len(str_html)==0:
         raise Exception
 
-    h = HTMLParser.HTMLParser()
-    str_from_txt = h.unescape(str_html[0]).strip()
+    h = html.parser.HTMLParser()
+    str_from_txt = h.unescape(str_html[0].decode("utf-8")).strip()
     return str_from_txt
 
 
@@ -343,29 +347,30 @@ def find_cur_KMNI(url):
     """
     find the names of the current files of the images of analysis and prediction of the Dutch Weather service
     """
-    aResp = urllib2.urlopen(url)
+    aResp = urllib.request.urlopen(url)
     web_pg = aResp.read()
-    re_cur_inds = 'href="//cdn.knmi.nl/knmi/map/page/weer/waarschuwingen_verwachtingen/weerkaarten/([APL]*[0-9]*)_large.gif"'
+    re_cur_inds = b'href="//cdn.knmi.nl/knmi/map/page/weer/waarschuwingen_verwachtingen/weerkaarten/([APL]*[0-9]*)_large.gif"'
     cur_IDs = re.findall(re_cur_inds, web_pg)
 
     if len(cur_IDs) !=4:
         # previously there were 4 maps available (1 analysis, 3 predictions)
         # this is not the case anymore
         # hence, an error is printed, not an exception raised
-        print "=== !!! ==="
-        print "The Dutch don't have 4 maps available, as they usually do"
-        print "=== !!! ==="
+        print("=== !!! ===")
+        print("The Dutch don't have 4 maps available, as they usually do")
+        print("=== !!! ===")
         # raise Exception
 
     return cur_IDs
 
 
 def find_cur_ZAMG_img_url(url):
-    aResp = urllib2.urlopen(url)
+    aResp = urllib.request.urlopen(url)
     web_pg = aResp.read()
-    zamg_re = "<img src=\"(http://www.zamg.ac.at/fix/wetter/bodenkarte/[0-9]*/[0-9]*/[0-9]*/BK_BodAna_Sat_([0-9]*).png)\" border=\"0\" />"
-    cur_id = re.findall(zamg_re, web_pg)
-    return cur_id[0][0], cur_id[0][1]
+    zamg_re = b"<img src=\"(http://www.zamg.ac.at/fix/wetter/bodenkarte/[0-9]*/[0-9]*/[0-9]*/BK_BodAna_Sat_([0-9]*).png)\" border=\"0\" />"
+    cur_id = re.findall(zamg_re, 
+                        web_pg)
+    return cur_id[0][0].decode("utf-8"), cur_id[0][1].decode("utf-8")
 
 
 if __name__ == '__main__':
